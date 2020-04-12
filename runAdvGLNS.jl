e055132e5dced1b87cde372b826733f0fbdbe6e4
Andrei Kudryavtsev <andreikudrya1995@gmail.com>
2020-04-12 19:19:09 +0500

    Added GTSPLIB.

--- runAdvGLNS.jl
+++ runAdvGLNS.jl
@@ -1,20 +1,9 @@
 #!/usr/bin/env julia
 
-# Copyright 2017 Stephen L. Smith and Frank Imeson
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-using GLNS
+# TODO: Register package.
+include("src/AdvancedGLNS.jl")
+
 
 """
 Optional Flags -- values are given in square brackets []  
@@ -86,6 +75,4 @@ end
 
 # running the code on the problem instance passed in via command line args.
 problem_instance, optional_args = parse_cmd(ARGS)
-GLNS.solver(problem_instance; optional_args...)
-
-
+AdvancedGLNS.solver(problem_instance; optional_args...)
--- src/AdvancedGLNS.jl
+++ src/AdvancedGLNS.jl
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module GLNS
+module AdvancedGLNS
 export solver
 using Random
 include("utilities.jl")
