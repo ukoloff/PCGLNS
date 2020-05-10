3c7022da785f5f5c83f651e432df4d64b9aa64b6
Andrei Kudryavtsev <andreikudrya1995@gmail.com>
2020-05-11 04:19:00 +0500

    Pass number of desired threads as script param.

--- runPCGLNS.bat
+++ runPCGLNS.bat
@@ -1,2 +1,3 @@
-set JULIA_NUM_THREADS=%NUMBER_OF_PROCESSORS%
-julia --check-bounds=no runPCGLNS.jl %*
+set JULIA_NUM_THREADS=%1
+for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+julia --check-bounds=no runPCGLNS.jl %ALL_BUT_FIRST%
--- runPCGLNS.sh
+++ runPCGLNS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export JULIA_NUM_THREADS=$(nproc)
-julia --check-bounds=no runPCGLNS.jl "$@"
+export JULIA_NUM_THREADS=$1
+julia --check-bounds=no runPCGLNS.jl "${@:2}"
