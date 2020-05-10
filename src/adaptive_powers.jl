eed4769745b60313e9cd0e2340d09b677f918656
Andrei Kudryavtsev <andreikudrya1995@gmail.com>
2020-05-11 00:54:34 +0500

    Moved to Apache License, Version 2.0.

--- src/adaptive_powers.jl
+++ src/adaptive_powers.jl
@@ -1,4 +1,5 @@
 # Copyright 2017 Stephen L. Smith and Frank Imeson
+# Modified copyright 2020 A. Kudryavtsev and M. Khachay
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 ## The items needed to adapt the powers in pdf_insert and pdf_remove
 """
