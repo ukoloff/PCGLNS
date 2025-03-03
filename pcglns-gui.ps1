632ab18e8f270ef2345b506c95e68fdc84d458d0
Stas Ukolov <ukoloff@gmail.com>
2025-03-03 14:47:55 +0500

    Add controls

--- pcglns-gui.ps1
+++ pcglns-gui.ps1
@@ -16,10 +16,36 @@ Add-Type -AssemblyName System.Windows.Forms
     WindowStartupLocation="CenterScreen"
     SizeToContent="WidthAndHeight"
 >
-  <StackPanel Orientation="Horizontal" HorizontalAlignment="Center" >
-    <Button Content="Go!" x:Name="btnGo" IsDefault="True" Padding="9 0"/>
-    <Button Content="Закрыть" IsCancel="True" Padding="9 0" Margin="7 0"/>
-  </StackPanel>
+<StackPanel Margin="5">
+    <TextBlock Text="Модель для решения" />
+    <Grid>
+        <Grid.ColumnDefinitions>
+        <ColumnDefinition Width="*"/>
+        <ColumnDefinition Width="Auto" />
+        </Grid.ColumnDefinitions>
+        <TextBox x:Name="src" MaxLength="50"  />
+        <Button x:Name="btnSrc" Grid.Column="1" Content="Обзор" Padding="5 0" />
+    </Grid>
+    <TextBlock Text="Результат" />
+    <Grid>
+        <Grid.ColumnDefinitions>
+        <ColumnDefinition Width="*"/>
+        <ColumnDefinition Width="Auto" />
+        </Grid.ColumnDefinitions>
+        <TextBox x:Name="dst" MaxLength="50"  />
+        <Button x:Name="btnDst" Grid.Column="1" Content="Обзор" Padding="5 0" />
+    </Grid>
+    <TextBlock Text="Режим" />
+    <ComboBox x:Name="mode" SelectedIndex="1" Padding="5 1" Margin="0 0 0 5">
+        <ComboBoxItem>Fast</ComboBoxItem>
+        <ComboBoxItem>Default</ComboBoxItem>
+        <ComboBoxItem>Slow</ComboBoxItem>
+    </ComboBox>
+    <StackPanel Orientation="Horizontal" HorizontalAlignment="Center" >
+        <Button Content="Go!" x:Name="btnGo" IsDefault="True" Padding="9 0"/>
+        <Button Content="Закрыть" IsCancel="True" Padding="9 0" Margin="7 0"/>
+    </StackPanel>
+</StackPanel>
 </Window>
 "@
 
